9c393205965fd6a92e5b13b23fa2c1085d611bb2
proje <alisamioglu00@gmail.com>
2023-06-07 04:17:39 +0300

    son hali

--- app/src/main/java/com/samioglu/newc/HomePage.kt
+++ app/src/main/java/com/samioglu/newc/HomePage.kt
@@ -3,19 +3,27 @@ package com.samioglu.newc
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.samioglu.newc.databinding.ActivityHomePageBinding
 
 class HomePage : AppCompatActivity() {
     private lateinit var binding: ActivityHomePageBinding
     private lateinit var listView: ListView
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomePageBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        auth = Firebase.auth
 
         listView = findViewById(R.id.listView)
 
@@ -47,5 +55,26 @@ class HomePage : AppCompatActivity() {
             }
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.menu,menu)
+
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.logout) {
+            auth.signOut()
+            val intent = Intent(this,LogIn::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
